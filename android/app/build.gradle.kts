7a3976a2040c8566b968d0e48a68f960ab213d5b
SoyKam <juancamiloeng@gmail.com>
2026-03-11 20:15:55 -0500

    Ajuste en la configuración del plugin de Google Services
    Se corrigió la implementación del plugin `com.google.gms.google-services` para asegurar su correcta aplicación en el proyecto:
    - Se movió la definición del plugin y su versión al archivo `build.gradle.kts` de nivel de proyecto (root).
    - Se aplicó efectivamente el plugin en el archivo `build.gradle.kts` del módulo `app`, eliminando la declaración previa que no lo ejecutaba.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-
-    // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
